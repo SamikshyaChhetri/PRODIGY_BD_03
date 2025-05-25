42d2d8cc6ee00a439d385f07b336ef75be94d303
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2025-05-25 18:43:26 +0545

    feat: add admin route to main application

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import cookieParser from "cookie-parser";
 import express from "express";
+import { adminRouter } from "./features/admin/route.js";
 import router from "./features/auth/route.js";
 import profileRouter from "./features/profile/route.js";
 import { PrismaClient } from "./generated/prisma/index.js";
@@ -9,6 +10,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use("/auth", router);
 app.use("/profile", profileRouter);
+app.use("/admin", adminRouter);
 export const prisma = new PrismaClient();
 app.listen(3000, () => {
   console.log("App is running");
