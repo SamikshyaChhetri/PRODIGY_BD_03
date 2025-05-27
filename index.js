4eb1b0d16e37ef7a4982d5b442317107355ba2ac
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2025-05-27 14:16:39 +0545

    feat: set up authentication route and update server port to 3000

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from "express";
+import router from "./features/auth/route.js";
 import { PrismaClient } from "./generated/prisma/index.js";
 
 const app = express();
 app.use(express.json());
+app.use("/auth", router);
 export const prisma = new PrismaClient();
-app.listen(3333, () => {
+app.listen(3000, () => {
   console.log("App is running");
 });
