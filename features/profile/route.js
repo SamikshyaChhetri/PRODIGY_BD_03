a7fc53e4a100894ea55b0ae3e4d6a406b9e323a6
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2025-05-30 18:42:44 +0545

    fix: remove isAdmin middleware from profile route

--- features/profile/route.js
+++ features/profile/route.js
@@ -1,9 +1,8 @@
 import { Router } from "express";
-import { isAdmin } from "../../middleware/isAdmin.js";
 import { isUser } from "../../middleware/isUser.js";
 
 const router = Router();
-router.get("/", isUser, isAdmin, (req, res) => {
+router.get("/", isUser, (req, res) => {
   return res.send("This is profile route");
 });
 export default router;
