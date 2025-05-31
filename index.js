1fdb5c397a36b3f39bf62a4eb364693a9e5dd43f
Samikshya Baniya Chhetri <118588912+SamikshyaChhetri@users.noreply.github.com>
2025-05-31 21:32:14 +0545

    feat: add cookie-parser middleware for handling cookies

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+import cookieParser from "cookie-parser";
 import express from "express";
 import router from "./features/auth/route.js";
 import { PrismaClient } from "./generated/prisma/index.js";
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 app.use("/auth", router);
 export const prisma = new PrismaClient();
 app.listen(3000, () => {
